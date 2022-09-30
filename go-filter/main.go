67e3f78ac8806e13d3b1f3e32d7f0735dd354427
youxingzhi <xingzhi.you@shopee.com>
2022-09-30 18:27:56 +0800

    why so slow!!!!

--- go-filter/main.go
+++ go-filter/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"syscall/js"
 	"unsafe"
 )
@@ -18,7 +17,6 @@ func parseKernel(kernel js.Value) [3][3]int {
 
 func copyBytesToGo(value js.Value) []byte {
 	len := value.Get("byteLength").Int()
-	fmt.Println(len)
 	data := make([]byte, len)
 	js.CopyBytesToGo(data, value)
 	return data
@@ -34,49 +32,62 @@ func initShareMemory(this js.Value, args []js.Value) any {
 	return js.ValueOf(boxedPtrMap)
 }
 
-func If(condition bool, trueVal, falseVal uint8) uint8 {
-	if condition {
-		return trueVal
+func getVal(val int) uint8 {
+	if val > 255 {
+		return 255
+	} else {
+		if val < 0 {
+			return 0
+		}
+		return uint8(val)
 	}
-	return falseVal
 }
 
 func filterByGOCopy(this js.Value, args []js.Value) any {
 	data := copyBytesToGo(args[0])
 	width := args[1].Int()
 	height := args[2].Int()
-	l := width * height
-	kernel := parseKernel(args[3])
+	// kernel := parseKernel(args[3])
 
-	fmt.Println(len(data), width, height, l*4, kernel)
-	w := len(kernel)
-	half := w / 2
+	// fmt.Println(data, width, height, kernel)
+	// w := len(kernel)
+	// half := w / 2
 
-	var ret []any
-	for y := 0; y < height-half; y++ {
-		for x := 0; x < width-half; x++ {
+	ret := make([]any, width*height*4, width*height*4)
+	// for y := half; y < height-half; y++ {
+	for y := 0; y < height; y++ {
+		// for x := half; x < width-half; x++ {
+		for x := 0; x < width; x++ {
 			px := (y*width + x) * 4 // pixel index.
-			r := 0
-			g := 0
-			b := 0
+			// r := 0
+			// g := 0
+			// b := 0
 
 			// core iteration.
-			for cy := 0; cy < w; cy++ {
-				for cx := 0; cx < w; cx++ {
-					cpx := ((y+(cy-half))*width + (x + (cx - half))) * 4
-					// if (px === 50216) debugger
-					r += int(data[cpx+0]) * kernel[cy][cx]
-					g += int(data[cpx+1]) * kernel[cy][cx]
-					b += int(data[cpx+2]) * kernel[cy][cx]
-					// a += data[cpx + 3] * kernel[cy][cx]
-				}
-			}
-			(ret)[px+0] = If(r > 255, 255, If(r < 0, 0, uint8(r)))
-			(ret)[px+1] = If(g > 255, 255, If(g < 0, 0, uint8(g)))
-			(ret)[px+2] = If(b > 255, 255, If(b < 0, 0, uint8(b)))
+			// for cy := 0; cy < w; cy++ {
+			// 	for cx := 0; cx < w; cx++ {
+			// 		cpx := ((y+(cy-half))*width + (x + (cx - half))) * 4
+			// 		// if (px === 50216) debugger
+			// 		r += int(data[cpx+0]) * kernel[cy][cx]
+			// 		g += int(data[cpx+1]) * kernel[cy][cx]
+			// 		b += int(data[cpx+2]) * kernel[cy][cx]
+			// 		// a += data[cpx + 3] * kernel[cy][cx]
+			// 	}
+			// }
+			// fmt.Println(r, g, b)
+			// ret[px+0] = getVal(r)
+			// ret[px+1] = getVal(g)
+			// ret[px+2] = getVal(b)
+
+			ret[px+0] = data[px+0]
+			ret[px+1] = data[px+1]
+			ret[px+2] = data[px+2]
+			ret[px+3] = data[px+3]
 		}
 
 	}
+	// fmt.Println(ret)
+
 	return js.ValueOf(ret)
 }
 
